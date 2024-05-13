b13bde8af06ed71ee4de012ca062aa04bab8e460
konira <loribao@hotmail.com>
2024-05-12 22:54:04 -0300

    correção conflito

--- src/appsec-desktop/src/View/routes.tsx
+++ src/appsec-desktop/src/View/routes.tsx
@@ -1,13 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Login from "./pages/Login";
 import Projeto from "./pages/Projects";
 import MyProfile from "./pages/MyProfile";
-import PageClimp from "./components/PageClimp";
-import Modal from "./components/Modal";
-import { Header } from "./components/Header";
-import Footer from "./components/Footer";
-import  MenuDock  from "./components/MenuDock";
 
 export const router = createBrowserRouter([
     {
